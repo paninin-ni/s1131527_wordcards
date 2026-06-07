c2251a4dcd3ccbe012130d61e82c6833b6b68bc8
品岑 周 <245joymelissa@gmail.com>
2026-06-08 05:55:18 +0800

    Update LICENSE and UI enhancements for frmEditWord
    The `LICENSE.txt` file was updated to include a copyright notice for 2026 under the name "panini".
    
    In `frmEditWord.Designer.cs`, a `PictureBox` control (`picLogo`) was added to display the `WordCards_Logo` image. Adjustments were made to the `grpWord` and `grpPhonogram` group boxes, including changes to their `ForeColor` properties and size reductions. The `txtWord` and `txtPhonogram` text boxes were resized, and the `btnSave` button's location was slightly adjusted. The form's background color was updated, and the `Enabled` property was removed.
    
    Initialization and cleanup code for `picLogo` was added, along with a new field definition for the control.
    
    In `frmWordCards.Designer.cs`, the `StartPosition` property was set to `CenterScreen`.

--- s1131527_wordcards/frmEditWord.Designer.cs
+++ s1131527_wordcards/frmEditWord.Designer.cs
@@ -37,19 +37,22 @@
             this.grpExplain = new System.Windows.Forms.GroupBox();
             this.txtExplain = new System.Windows.Forms.TextBox();
             this.btnSave = new System.Windows.Forms.Button();
+            this.picLogo = new System.Windows.Forms.PictureBox();
             this.grpWord.SuspendLayout();
             this.grpPhonogram.SuspendLayout();
             this.grpSoundPath.SuspendLayout();
             this.grpExplain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
             this.SuspendLayout();
             // 
             // grpWord
             // 
             this.grpWord.Controls.Add(this.txtWord);
             this.grpWord.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.grpWord.ForeColor = System.Drawing.SystemColors.HotTrack;
             this.grpWord.Location = new System.Drawing.Point(43, 39);
             this.grpWord.Name = "grpWord";
-            this.grpWord.Size = new System.Drawing.Size(706, 137);
+            this.grpWord.Size = new System.Drawing.Size(499, 137);
             this.grpWord.TabIndex = 0;
             this.grpWord.TabStop = false;
             this.grpWord.Text = "單字";
@@ -58,16 +61,17 @@
             // 
             this.txtWord.Location = new System.Drawing.Point(21, 48);
             this.txtWord.Name = "txtWord";
-            this.txtWord.Size = new System.Drawing.Size(657, 50);
+            this.txtWord.Size = new System.Drawing.Size(450, 50);
             this.txtWord.TabIndex = 0;
             // 
             // grpPhonogram
             // 
             this.grpPhonogram.Controls.Add(this.txtPhonogram);
             this.grpPhonogram.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.grpPhonogram.ForeColor = System.Drawing.Color.Green;
             this.grpPhonogram.Location = new System.Drawing.Point(43, 199);
             this.grpPhonogram.Name = "grpPhonogram";
-            this.grpPhonogram.Size = new System.Drawing.Size(706, 137);
+            this.grpPhonogram.Size = new System.Drawing.Size(499, 137);
             this.grpPhonogram.TabIndex = 1;
             this.grpPhonogram.TabStop = false;
             this.grpPhonogram.Text = "音標";
@@ -76,7 +80,7 @@
             // 
             this.txtPhonogram.Location = new System.Drawing.Point(21, 48);
             this.txtPhonogram.Name = "txtPhonogram";
-            this.txtPhonogram.Size = new System.Drawing.Size(657, 50);
+            this.txtPhonogram.Size = new System.Drawing.Size(450, 50);
             this.txtPhonogram.TabIndex = 0;
             // 
             // grpSoundPath
@@ -120,7 +124,7 @@
             // 
             this.btnSave.DialogResult = System.Windows.Forms.DialogResult.Yes;
             this.btnSave.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSave.Location = new System.Drawing.Point(595, 912);
+            this.btnSave.Location = new System.Drawing.Point(595, 924);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(154, 60);
             this.btnSave.TabIndex = 4;
@@ -128,17 +132,29 @@
             this.btnSave.UseVisualStyleBackColor = true;
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
+            // picLogo
+            // 
+            this.picLogo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.picLogo.Image = global::s1131527_wordcards.Properties.Resources.WordCards_Logo;
+            this.picLogo.Location = new System.Drawing.Point(566, 61);
+            this.picLogo.Name = "picLogo";
+            this.picLogo.Size = new System.Drawing.Size(183, 251);
+            this.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picLogo.TabIndex = 5;
+            this.picLogo.TabStop = false;
+            // 
             // frmEditWord
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(254)))), ((int)(((byte)(242)))));
             this.ClientSize = new System.Drawing.Size(779, 1025);
+            this.Controls.Add(this.picLogo);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.grpExplain);
             this.Controls.Add(this.grpSoundPath);
             this.Controls.Add(this.grpPhonogram);
             this.Controls.Add(this.grpWord);
-            this.Enabled = false;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MinimizeBox = false;
             this.Name = "frmEditWord";
@@ -153,6 +169,7 @@
             this.grpSoundPath.PerformLayout();
             this.grpExplain.ResumeLayout(false);
             this.grpExplain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -168,5 +185,6 @@
         private System.Windows.Forms.GroupBox grpExplain;
         private System.Windows.Forms.TextBox txtExplain;
         private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.PictureBox picLogo;
     }
 }
--- s1131527_wordcards/frmWordCards.Designer.cs
+++ s1131527_wordcards/frmWordCards.Designer.cs
@@ -192,6 +192,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.KeyPreview = true;
             this.Name = "frmWordCards";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "單字卡";
             this.Load += new System.EventHandler(this.frmWordCards_Load);
             this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.frmWordCards_KeyPress);
